0a433180a5e9aec52b0f66b639f67ea8a47e3e4d
altenull <altenull@gmail.com>
2019-03-14 22:47:31 +0900

    chore: add contents and images

---
+++ src/assets/contents/footty.content.ts
@@ -0,0 +1,10 @@
+import { leagueContents } from './leagueContents';
+
+export const foottyContents = {
+  en: {
+    league: leagueContents.en,
+  },
+  ko: {
+    league: leagueContents.ko,
+  },
+};
