262a7adab297ac00b2a34fbaa1ff96c442a1c477
altenull <altenull@gmail.com>
2019-04-21 16:09:12 +0900

    feat(league): create LeagueTable component

---
+++ src/app/league/components/LeagueTable.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+import { withLocale } from '../../../contexts/locale.context';
+
+interface Props {
+  localizedContents: any;
+  tableRows: React.ReactNode;
+}
+
+const StLeagueTable = styled.table`
+  color: #ffffff;
+  font-size: 1rem;
+  background-color: rgba(0, 0, 0, 0.25);
+  margin: 80px 0;
+`;
+
+class LeagueTable extends React.Component<Props> {
+  render() {
+    const { localizedContents, tableRows } = this.props;
+    const leagueTableContents = localizedContents.league.leagueTable;
+
+    return (
+      <StLeagueTable cellPadding={'6'}>
+        <thead>
+          <tr>
+            <th>{leagueTableContents.position}</th>
+            <th>{leagueTableContents.club}</th>
+            <th>{leagueTableContents.played}</th>
+            <th>{leagueTableContents.won}</th>
+            <th>{leagueTableContents.drawn}</th>
+            <th>{leagueTableContents.lost}</th>
+            <th>{leagueTableContents.goalsFor}</th>
+            <th>{leagueTableContents.goalsAgainst}</th>
+            <th>{leagueTableContents.goalDifference}</th>
+            <th>{leagueTableContents.next}</th>
+            <th>{leagueTableContents.points}</th>
+          </tr>
+        </thead>
+        <tbody>{tableRows}</tbody>
+      </StLeagueTable>
+    );
+  }
+}
+
+export default withLocale(LeagueTable);
