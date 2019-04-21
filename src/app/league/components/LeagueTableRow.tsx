310bfac2d1c4f1fead0183d0fc321307f488852f
altenull <altenull@gmail.com>
2019-04-21 16:17:21 +0900

    feat(league): create LeagueTableRow component

---
+++ src/app/league/components/LeagueTableRow.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+import { ObjectizedLeagueTable } from '../../../store/models/foottyAPI/foottyAPI-league.store.model';
+import { NextEvent } from '../models/league.model';
+
+interface Props {
+  teamId: string;
+  rank: number;
+  badgeUrl: string | null;
+  data: ObjectizedLeagueTable;
+  nextEvent: NextEvent | null;
+  onSelectTeam(teamId: string): void;
+}
+
+const StLeagueTableRow = styled.tr`
+  font-size: 1.25rem;
+  line-height: 3;
+  cursor: pointer;
+`;
+
+const StLeagueTableTd = styled.td`
+  text-align: 'center';
+`;
+
+class LeagueTableRow extends React.Component<Props> {
+  render() {
+    const { teamId, rank, badgeUrl, data, nextEvent, onSelectTeam } = this.props;
+
+    const handledGoalsDifference: string =
+      data.goalsdifference > 0 ? `+${data.goalsdifference}` : `${data.goalsdifference}`;
+
+    return (
+      <StLeagueTableRow onClick={() => onSelectTeam(teamId)}>
+        <th>{rank}</th>
+        <StLeagueTableTd style={{ textAlign: 'left' }}>
+          <span>{badgeUrl ? <img src={badgeUrl} width={'36px'} /> : 'No image'}</span> {data.name}
+        </StLeagueTableTd>
+        <StLeagueTableTd>{data.played}</StLeagueTableTd>
+        <StLeagueTableTd>{data.win}</StLeagueTableTd>
+        <StLeagueTableTd>{data.draw}</StLeagueTableTd>
+        <StLeagueTableTd>{data.loss}</StLeagueTableTd>
+        <StLeagueTableTd>{data.goalsfor}</StLeagueTableTd>
+        <StLeagueTableTd>{data.goalsagainst}</StLeagueTableTd>
+        <StLeagueTableTd>{handledGoalsDifference}</StLeagueTableTd>
+        <StLeagueTableTd>
+          <span>
+            {nextEvent != null && nextEvent.badgeUrl ? <img src={nextEvent.badgeUrl} width={'36px'} /> : 'No image'}
+          </span>
+        </StLeagueTableTd>
+        <StLeagueTableTd>{data.total}</StLeagueTableTd>
+      </StLeagueTableRow>
+    );
+  }
+}
+
+export default LeagueTableRow;
